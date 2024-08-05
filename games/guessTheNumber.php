57d52fdd705101d1f5d691b314be2e00a9e38730
unknown <iliya.souri7890@gmail.com>
2024-08-05 16:32:58 +0330

    opening html at shortcode

--- games/guessTheNumber.php
+++ games/guessTheNumber.php
@@ -1,8 +1,11 @@
 <?php
 function guess_the_number() {
     $correctNumber = rand(1,3);
-    $htmlFileContent = fgets(fopen("templates\index.html","r"));
+    $htmlFile = fopen(dirname(dirname(__FILE__))."\templates\index.html","r");
+    $htmlFileContent = fgets($htmlFile);
+    fclose($htmlFile);
     return $htmlFileContent;
 }
+
 
 add_shortcode("numberGuess", 'guess_the_number');
