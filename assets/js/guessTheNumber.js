ddd97cbd037859787483f146098dc38e914aae18
unknown <iliya.souri7890@gmail.com>
2024-08-05 17:51:07 +0330

    Adding confetti for winning GTN

--- assets/js/guessTheNumber.js
+++ assets/js/guessTheNumber.js
@@ -10,6 +10,9 @@ document.getElementById("zen-guess-the-number-input").addEventListener("input",f
     var humanGuessedNumber = document.getElementById("zen-guess-the-number-input").value;
     if (humanGuessedNumber == document.getElementById("zen-guess-the-number-input").getAttribute("hint")) 
     {
+        party.confetti(getElementById("zen-guess-the-number-input"), {
+            count: party.variation.range(20, 40)
+        });
         console.log("You got it!")
     }
     else
