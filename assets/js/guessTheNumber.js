45ae40a97ba532581e9cf57e8c37ef50519f7053
unknown <iliya.souri7890@gmail.com>
2024-08-05 17:38:56 +0330

    OnInput got out of onLoad

--- assets/js/guessTheNumber.js
+++ assets/js/guessTheNumber.js
@@ -3,16 +3,17 @@ console.log("GTN JS is loaded")
 
 document.addEventListener("load",function () {
     console.log("document is loaded");
-    document.getElementById("zen-guess-the-number-input").addEventListener("oninput",function () {
-        console.log("input is changed");
-        var humanGuessedNumber = document.getElementById("zen-guess-the-number-input").value;
-        if (humanGuessedNumber == document.getElementById("zen-guess-the-number-input").getAttribute("hint")) 
-        {
-            console.log("You got it!")
-        }
-        else
-        {
-            console.log("Not quite correct")
-        }
-    })
+    
+})
+document.getElementById("zen-guess-the-number-input").addEventListener("oninput",function () {
+    console.log("input is changed");
+    var humanGuessedNumber = document.getElementById("zen-guess-the-number-input").value;
+    if (humanGuessedNumber == document.getElementById("zen-guess-the-number-input").getAttribute("hint")) 
+    {
+        console.log("You got it!")
+    }
+    else
+    {
+        console.log("Not quite correct")
+    }
 })
