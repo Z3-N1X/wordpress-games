355e5bed328073618ac3ff1a4ed39aa2c85268e1
unknown <iliya.souri7890@gmail.com>
2024-08-05 16:24:57 +0330

    Adding the guess the number short code

--- games/guessTheNumber.php
+++ games/guessTheNumber.php
@@ -1,7 +1,8 @@
 <?php
 function guess_the_number() {
     $correctNumber = rand(1,3);
-    return '<input hint="'.$correctNumber.'" id="z3n-guess-the-number">';
+    $htmlFileContent = fgets(fopen("templates\index.html","r"));
+    return $htmlFileContent;
 }
 
 add_shortcode("numberGuess", 'guess_the_number');
