b10b74404569ce16a8156e27e451aa34f6d09461
unknown <iliya.souri7890@gmail.com>
2024-08-06 16:50:29 +0330

    forgot '/'

--- games/flappyBird.php
+++ games/flappyBird.php
@@ -3,8 +3,8 @@ function flappy_bird() {
     $htmlFile = fopen(dirname(dirname(__FILE__)).'/templates/flappyHtml.html',"r");
     $htmlFileContent = fgets($htmlFile);
     fclose($htmlFile);
-    wp_enqueue_script("flappybird", 'wp-content/plugins/gamePluginZ3N/assets/js/flappyBird.js');
-    wp_enqueue_style("flappybird", 'wp-content/plugins/gamePluginZ3N/assets/css/flappyBirdStyle.css');
+    wp_enqueue_script("flappybird", '/wp-content/plugins/gamePluginZ3N/assets/js/flappyBird.js');
+    wp_enqueue_style("flappybird", '/wp-content/plugins/gamePluginZ3N/assets/css/flappyBirdStyle.css');
     return $htmlFileContent;
 }
 
