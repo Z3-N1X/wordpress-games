2103e1646e5c71bd00932682607d0bbe7784e4a6
unknown <iliya.souri7890@gmail.com>
2024-08-06 09:58:33 +0330

    fixed path

--- games/flappyBird.php
+++ games/flappyBird.php
@@ -3,7 +3,7 @@ function flappy_bird() {
     $htmlFile = fopen(dirname(dirname(__FILE__)).'/templates/flappyHtml.html',"r");
     $htmlFileContent = fgets($htmlFile);
     fclose($htmlFile);
-    wp_enqueue_script("guessthenumber", "/wp-content/plugins/zenGamePlugin/assets/js/flappyBird.js");
+    wp_enqueue_script("guessthenumber", "/wp-content/plugins/gamePluginZ3N/assets/js/flappyBird.js");
     return $htmlFileContent;
 }
 
