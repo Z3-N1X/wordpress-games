b559ccc09d061e1d616c43abf88ec099e7930acf
unknown <iliya.souri7890@gmail.com>
2024-08-06 16:32:34 +0330

    fixed strings

--- games/flappyBird.php
+++ games/flappyBird.php
@@ -3,8 +3,8 @@ function flappy_bird() {
     $htmlFile = fopen(dirname(dirname(__FILE__)).'/templates/flappyHtml.html',"r");
     $htmlFileContent = fgets($htmlFile);
     fclose($htmlFile);
-    wp_enqueue_script("flappybird", dirname(dirname(__FILE__))."/assets/js/flappyBird.js");
-    wp_enqueue_style("flappybird", dirname(dirname(__FILE__))."/assets/css/flappyBirdStyle.css");
+    wp_enqueue_script("flappybird", dirname(dirname(__FILE__)).'/assets/js/flappyBird.js');
+    wp_enqueue_style("flappybird", dirname(dirname(__FILE__)).'/assets/css/flappyBirdStyle.css');
     return $htmlFileContent;
 }
 
